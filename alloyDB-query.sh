11167b9312c50ccb602894b078d844e7705fba4e
Sarun singla <98247467+sarunsingla11722@users.noreply.github.com>
2024-09-11 10:52:45 -0700

    Update alloyDB-query.sh

--- alloyDB-query.sh
+++ alloyDB-query.sh
@@ -12,7 +12,7 @@ start=$(date +%s)
 for i in {1..100000}
 do
   start=$(date +%s%3N)
-  psql -h 10.34.1.9 -U postgres -d ssb -f /home/sarunsingla/s64da-benchmark-toolkit/benchmarks/ssb/queries/Q1.3.sql 
+  psql -h <private IP of read replica> -U postgres -d ssb -f /home/sarunsingla/s64da-benchmark-toolkit/benchmarks/ssb/queries/Q1.3.sql 
   end=$(date +%s%3N)
   difference=$((end - start))
   echo "$difference"
