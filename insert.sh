23ba8f3111c5e4d090ecb24685daaedb18bdb1a2
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2025-04-15 09:26:19 +0200

    Update insert.sh

--- insert.sh
+++ insert.sh
@@ -1,9 +1,3 @@
-N=10
 DB_NAME="ssb"
 source ~/pgauth.env
-
-for (( i=1; i<=N; i++ ))
-do
-  echo "Starting job $i..."
-  psql -d "$DB_NAME" -f ./insertpg.sql &
-done
+psql -d "$DB_NAME" -f ./insertpg.sql
