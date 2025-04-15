d9ff7afdac62106901e1bcb9455521db8df083eb
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2025-04-15 10:22:54 +0200

    Update insertpg.sql

--- insertpg.sql
+++ insertpg.sql
@@ -1,14 +1,27 @@
 DO
 $do$
 DECLARE
-  lo_orderkey INTEGER;
-  max_rows INTEGER := 10000;
+  max_rows INTEGER := 100000;
 BEGIN
-  SELECT COUNT(*) from lineorder into lo_orderkey;
-  RAISE NOTICE 'current order#: %', lo_orderkey;
   WHILE max_rows > 0 LOOP
-    lo_orderkey := lo_orderkey + 1;
-    insert into lineorder values(200000000+lo_orderkey,1,73801,374973,9241,19940207,'5-LOW',0,30,614388,11435375,6,565236,122877,7,19940629,'RAIL');    
+    insert into lineorder (
+      lo_linenumber,
+      lo_custkey,
+      lo_partkey,
+      lo_suppkey,
+      lo_orderdate,
+      lo_orderpriority,
+      lo_shippriority,
+      lo_quantity,
+      lo_extendedprice,
+      lo_ordertotalprice,
+      lo_discount,
+      lo_revenue,
+      lo_supplycost,
+      lo_tax,
+      lo_commitdate,
+      lo_shipmode
+    ) values(1,73801,374973,9241,19940207,'5-LOW',0,30,614388,11435375,6,565236,122877,7,19940629,'RAIL');    
     commit;
     max_rows := max_rows - 1;
     RAISE NOTICE 'new order#: %', lo_orderkey;
